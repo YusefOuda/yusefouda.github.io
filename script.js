e0e9fd9ff606dd829f2af16e4ea7ae6fb9340a70
Yusef Ouda <yiouda92@gmail.com>
2020-11-09 23:19:27 -0600

    adds email to portfolio page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    let text = `~Hi there!~ My name is Yusef Ouda.~\n\nI am a software developer.~\n\nFind out more about me on `;
+    let text = `~Hi there!~ My name is Yusef Ouda.~\n\nI am a software developer.~\n\nFind out more about me on [Stack Overflow](https://stackoverflow.com/cv/yusefouda) or [contact me!](mailto:me@yusefouda.com)`;
     let textNode = document.createTextNode("");
     document.getElementById("container").insertBefore(textNode, document.getElementById("cursor"));
     var skip = false;
@@ -23,36 +23,29 @@ window.onload = () => {
                 document.getElementById("container").insertBefore(document.createElement("br"), document.getElementById("cursor"));
                 document.getElementById("container").insertBefore(node, document.getElementById("cursor"));
             }
-            else if (char !== "~") {
+            else if (char === "[") {
+                const start = i+1;
+                const end = text.substring(i+1).indexOf("]") + start;
+                const linkText = text.substring(start, end);
+                const urlStart = text.indexOf("(", end) + 1;
+                const urlEnd = text.indexOf(")", end);
+                const linkUrl = text.substring(urlStart, urlEnd);
+                let aNode = document.createElement("a");
+                aNode.href = linkUrl;
+                aNode.innerText = linkText;
+                aNode.target = "_blank";
+                document.getElementById("container").insertBefore(aNode, document.getElementById("cursor"));
+                text = text.replace(text.substring(start-1, urlEnd), "");
+                node = document.createTextNode("");
+                document.getElementById("container").insertBefore(node, document.getElementById("cursor"));
+            } else if (char !== "~") {
                 node.nodeValue = node.nodeValue + char;
-            } if (++i < text.length) {
+            }
+            
+            if (++i < text.length) {
                 printText(text, node, i);
-            } else {
-                insertLink();
             }
         }, delay);
     }
 
-    function insertLink() {
-        let text = "Stack Overflow.";
-        let textNode = document.createElement("a");
-        textNode.href = "https://stackoverflow.com/cv/yusefouda";
-        document.getElementById("container").insertBefore(textNode, document.getElementById("cursor"));
-        printLink(text, textNode, 0);
-    }
-
-    function printLink(text, node, i) {
-        let delay = 90;
-        if (skip) delay = delay / 20;
-        const char = text[i];
-        node.innerText += char;
-        setTimeout(() => {
-            node.nodeValue = node.nodeValue + char;
-
-            if (++i < text.length)
-                printLink(text, node, i);
-            else
-                document.getElementById("skip").style.visibility = "hidden";
-        }, delay);
-    }
 }
